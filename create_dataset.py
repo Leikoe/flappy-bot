a752976f24b27d02fa903665fde72de5c7116bd1
tristanretali <tristan.retali@gmail.com>
2023-02-20 12:24:29 +0100

    fix: :bug: Fixed add in no jump directory

--- create_dataset.py
+++ create_dataset.py
@@ -3,22 +3,25 @@ import pyautogui
 import keyboard
 
 
-def space_pressed(event) -> bool:
-    return event.event_type == keyboard.KEY_DOWN and event.name == 'space'
-
-
-# i = 0
-# while True:
-#     pyautogui.screenshot(f'img/test{i}.png')
-#     i += 1
-#     time.sleep(5)
+# def space_pressed(event) -> bool:
+#     return event.event_type == keyboard.KEY_DOWN and event.name == 'space'
 
 
 def main():
+    jump = 0
+    no_jump = 0
+    time.sleep(7)
     while True:
-        event = keyboard.read_event()
-        if space_pressed(event):
-            print('space was pressed')
+        time.sleep(0.03)
+        # event = keyboard.is_pressed()
+        if keyboard.is_pressed('space'):
+            pyautogui.screenshot(f'jump/jump_screenshot{jump}.png')
+            jump += 1
+            print('Added screenshot in jump directory')
+        else:
+            pyautogui.screenshot(f'no_jump/no_jump_screenshot{no_jump}.png')
+            no_jump += 1
+            print('Added screenshot in no jump directory')
 
 
 if __name__ == "__main__":
