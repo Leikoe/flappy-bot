0c2001007e65a32eec5646f3f19cd164e4366aef
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-02-22 14:17:47 +0100

    feat: added autodetect for game window

--- create_dataset.py
+++ create_dataset.py
@@ -4,6 +4,7 @@ import uuid
 import logging
 import os
 import pyautogui
+import pyscreeze
 
 DATASET_FOLDER = "dataset"
 
@@ -13,11 +14,17 @@ if __name__ == "__main__":
     if not os.path.isdir(f"{DATASET_FOLDER}/"):
         os.mkdir(DATASET_FOLDER)
 
-    time.sleep(5)
-    while True:
-        time.sleep(1./30.)
-        # event = keyboard.is_pressed()
-        if keyboard.is_pressed('space'):
-            pyautogui.screenshot(f'{DATASET_FOLDER}/{uuid.uuid4()}.png')
-        else:
-            pyautogui.screenshot(f'{DATASET_FOLDER}/{uuid.uuid4()}.png')
+    time.sleep(3)
+    x, y = pyscreeze.locateCenterOnScreen(image="top_left.png")
+    bottom_right = pyscreeze.locateCenterOnScreen(image="bottom_right.png")
+
+    print(f"detected game's top left corner at {x,y, bottom_right}")
+    # region = (x, y, 800, 1000)
+    #
+    # while True:
+    #     time.sleep(1./30.)
+    #     # event = keyboard.is_pressed()
+    #     if keyboard.is_pressed('space'):
+    #         pyautogui.screenshot(f'{DATASET_FOLDER}/{uuid.uuid4()}.png', region=region)
+    #     else:
+    #         pyautogui.screenshot(f'{DATASET_FOLDER}/{uuid.uuid4()}.png', region=region)
