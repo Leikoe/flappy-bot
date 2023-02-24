3aafe59627558c84c4038585cafbb30e3cbec175
tristanretali <tristan.retali@gmail.com>
2023-02-24 18:06:26 +0100

    feat: :sparkles: Added screenshot for a specific area

--- create_dataset.py
+++ create_dataset.py
@@ -3,8 +3,8 @@ import pyautogui
 import keyboard
 
 
-# def space_pressed(event) -> bool:
-#     return event.event_type == keyboard.KEY_DOWN and event.name == 'space'
+def take_screen(i: int, path: str):
+    pyautogui.screenshot(path, region=(0, 125, 800, 1200))
 
 
 def main():
@@ -13,13 +13,13 @@ def main():
     time.sleep(7)
     while True:
         time.sleep(0.03)
-        # event = keyboard.is_pressed()
         if keyboard.is_pressed('space'):
-            pyautogui.screenshot(f'jump/jump_screenshot{jump}.png')
+            take_screen(jump, f'dataset/jump/jump_screenshot{jump}.png')
             jump += 1
             print('Added screenshot in jump directory')
         else:
-            pyautogui.screenshot(f'no_jump/no_jump_screenshot{no_jump}.png')
+            take_screen(
+                no_jump, f'dataset/no_jump/no_jump_screenshot{no_jump}.png')
             no_jump += 1
             print('Added screenshot in no jump directory')
 
