406561fe067c3bf8c7473d97be7d8cdf5c5923da
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-02-25 14:16:08 +0100

    feat: working train.py

--- train.py
+++ train.py
@@ -1,28 +1,73 @@
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
+import os
+from PIL import Image
+from numpy import asarray
+
+# jump = 1, no_jump = 0
+xs = []
+ys = []
+
+DATASET_DIR = "dataset"
+for filename in os.listdir(f"{DATASET_DIR}/jump"):
+    f = os.path.join(f"{DATASET_DIR}/jump", filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        img = Image.open(f).convert('L')
+        numpydata = asarray(img)
+        xs.append(numpydata)
+        ys.append(1)
+
+for filename in os.listdir(f"{DATASET_DIR}/no_jump"):
+    f = os.path.join(f"{DATASET_DIR}/no_jump", filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        img = Image.open(f).convert('L')
+        numpydata = asarray(img)
+        xs.append(numpydata)
+        ys.append(0)
+
+xs = np.array(xs)
+ys = np.array(ys)
+
+print(f"number of images: {xs.shape[0]}")
+print(f"number of labels: {ys.shape[0]}")
+
+from matplotlib import pyplot as plt
+plt.imshow(xs[0], interpolation='nearest')
+plt.show()
 
 # Model / data parameters
-num_classes = 10
-input_shape = (28, 28, 1)
+input_shape = (600, 400, 1)
+
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+
+# shuffle the data
+xs, ys = unison_shuffled_copies(xs, ys)
 
 # Load the data and split it between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+TRAIN_SPLIT = 0.9
+TRAIN_IDX = int(xs.shape[0] * TRAIN_SPLIT)
+x_train, x_test = xs[:TRAIN_IDX], xs[TRAIN_IDX:]
+y_train, y_test = ys[:TRAIN_IDX], ys[TRAIN_IDX:]
+print(x_train.shape)
 
 # Scale images to the [0, 1] range
 x_train = x_train.astype("float32") / 255
 x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
+# Make sure images have shape (600, 400, 1)
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
 print("x_train shape:", x_train.shape)
 print(x_train.shape[0], "train samples")
 print(x_test.shape[0], "test samples")
 
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = keras.Sequential(
     [
@@ -33,18 +78,15 @@ model = keras.Sequential(
         layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Flatten(),
         layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
+        layers.Dense(1, activation="sigmoid"),
     ]
 )
 
 model.summary()
 
-batch_size = 128
 epochs = 15
-
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+model.compile(loss="mean_squared_error", optimizer="adam", metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=epochs, validation_split=0.1)
 
 
 score = model.evaluate(x_test, y_test, verbose=0)
