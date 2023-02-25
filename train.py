c4a24fbb793524bcd2a51034b47b289b8140c635
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-02-25 13:42:18 +0100

    feat: added train.py

--- train.py
+++ train.py
@@ -49,4 +49,4 @@ model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_spl
 
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
-print("Test accuracy:", score[1]
+print("Test accuracy:", score[1])
